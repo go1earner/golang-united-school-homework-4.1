755b28451ec43a44011c0371d32c33d7c8dec1fa
go1earner <mechetin.artur2011@yandex.com>
2022-05-15 14:19:16 +0300

    feat: add task solution 4.1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runeStr := []rune(input)
+	for i := len(runeStr); i != 0; i-- {
+		output += string(runeStr[i-1])
+	}
 	return output
 }
